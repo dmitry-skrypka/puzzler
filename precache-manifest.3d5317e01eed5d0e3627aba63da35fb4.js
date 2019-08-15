f0f421127309862a0370f4afbbfa6c8856c32dae
Dmitry Skrypka <dimitry.skripka@gmail.com>
2019-08-15 20:55:09 +0300

    Updates

---
+++ precache-manifest.3d5317e01eed5d0e3627aba63da35fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0ab1fd6ba3346f6e3d2dd4f6f5188e",
+    "url": "/puzzler/index.html"
+  },
+  {
+    "revision": "e38d1ff4057eb16e59b6",
+    "url": "/puzzler/static/css/main.826cf486.chunk.css"
+  },
+  {
+    "revision": "89429082f52af4f69681",
+    "url": "/puzzler/static/js/2.d71b4dd5.chunk.js"
+  },
+  {
+    "revision": "e38d1ff4057eb16e59b6",
+    "url": "/puzzler/static/js/main.0a4774bc.chunk.js"
+  },
+  {
+    "revision": "3d1d0006ec6640148111",
+    "url": "/puzzler/static/js/runtime~main.b257e4fe.js"
+  },
+  {
+    "revision": "8f3f5394511f00354616ed64c825e30f",
+    "url": "/puzzler/static/media/defaultPuzzleImage.8f3f5394.jpg"
+  },
+  {
+    "revision": "6b7a0ff1c7367b477a04e2d4aaf3d875",
+    "url": "/puzzler/static/media/solved.6b7a0ff1.gif"
+  }
+]);
